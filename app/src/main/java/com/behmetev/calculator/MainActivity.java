78db1688bbdd1bb65ea62af70c9c041ece32720c
Nikita Behmetev <nikitabehmetev@gmail.com>
2023-03-14 16:29:58 +0300

    Вывод рандомного примера

--- app/src/main/java/com/behmetev/calculator/MainActivity.java
+++ app/src/main/java/com/behmetev/calculator/MainActivity.java
@@ -19,17 +19,29 @@ public class MainActivity extends AppCompatActivity {
         Button button = findViewById(R.id.buttonAnswer);
         TextView wrongAnswer = findViewById(R.id.textViewInCorrectAnswer);
         TextView correctAnswer = findViewById(R.id.textViewCorrectAnswer);
+        TextView example = findViewById(R.id.textViewExample);
+
+        //подучаем рандомные целые числа от 1 до 99
+        int random_number1 = 1 + (int) (Math.random() * 99);
+        int random_number2 = 1 + (int) (Math.random() * 99);
+
+        example.setText(random_number1 + " + " + random_number2 + " = ?");
+
+        int answer = random_number1 + random_number2;
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String text = editTextAnswer.getText().toString();
-                if (text.equals("15")) {
+
+                //todo: переделать преобразование типов
+                //todo: рестарт активности с новыми данными
+                if (Integer.parseInt(text) == answer) {
                     correctAnswer.setVisibility(View.VISIBLE);
                     wrongAnswer.setVisibility(View.GONE);
                 } else {
-                    wrongAnswer.setVisibility(View.VISIBLE);
                     correctAnswer.setVisibility(View.GONE);
+                    wrongAnswer.setVisibility(View.VISIBLE);
                 }
             }
         });
