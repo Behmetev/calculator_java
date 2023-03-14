d934c4cc9eb0b5b5a4332bc030fc1a1313191377
Nikita Behmetev <nikitabehmetev@gmail.com>
2023-03-14 16:01:01 +0300

    Логика приложения

--- app/src/main/java/com/behmetev/calculator/MainActivity.java
+++ app/src/main/java/com/behmetev/calculator/MainActivity.java
@@ -3,6 +3,10 @@ package com.behmetev.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editTextAnswer = findViewById(R.id.editTextAnswer);
+        Button button = findViewById(R.id.buttonAnswer);
+        TextView wrongAnswer = findViewById(R.id.textViewInCorrectAnswer);
+        TextView correctAnswer = findViewById(R.id.textViewCorrectAnswer);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editTextAnswer.getText().toString();
+                if (text.equals("15")) {
+                    correctAnswer.setVisibility(View.VISIBLE);
+                    wrongAnswer.setVisibility(View.GONE);
+                } else {
+                    wrongAnswer.setVisibility(View.VISIBLE);
+                    correctAnswer.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
